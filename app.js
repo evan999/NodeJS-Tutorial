92a8f1c2aa3204043b9ee35076fce9f20ecce5a3
Evan Meshberg <emeshberge@gmail.com>
2018-09-13 03:18:01 +0000

    Created pipe to write to file

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 var http = require('http');
 var fs = require('fs');
 
-var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
-var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
 
-myRead
+
+var server = http.createServer(function(req, res){
+    console.log('request was made: ' + req.url);
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
+    myReadStream.pipe(res); 
+});
+
+server.listen(process.env.PORT, process.env.IP);
+console.log('yo dawgs, now listening to port');
+
+
+/*
 
 myReadStream.on('data', function(chunk){
     console.log('new chunk received');
@@ -12,7 +22,7 @@ myReadStream.on('data', function(chunk){
    // console.log(chunk);
 });
 
-
+*/
 
 
 
