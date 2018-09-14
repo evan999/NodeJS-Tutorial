009ac2b693ee9224722089978244d24514f87733
Evan Meshberg <emeshberge@gmail.com>
2018-09-14 01:13:12 +0000

    Added package.json file

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ var http = require('http');
 var fs = require('fs');
 
 
-
 var server = http.createServer(function(req, res){
     console.log('request was made: ' + req.url);
     if(req.url === '/home' || req.url === '/'){
         res.writeHead(200, {'Content-Type': 'text/html'});
         fs.createReadStream(__dirname + '/index.html').pipe(res);
     }
-    else if(req.url === '/contact'){
+    else if(req.url === '/contact-us'){
         res.writeHead(200, {'Content-Type': 'text/html'});
         fs.createReadStream(__dirname + '/contact.html').pipe(res);
     }
